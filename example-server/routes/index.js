78991f3e51c8bd239773b533a317de4abc158db8
zoelaventhol <110328502+zoelaventhol@users.noreply.github.com>
2024-10-28 15:13:57 +0100

    fix: removed example server

--- example-server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const getExample = require("../controllers/exampleController");
-
-/* GET example */
-router.get("/", getExample);
-
-module.exports = router;
